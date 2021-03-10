cb06bb8b88669e0cd84dfeeba2c673bf95ef9cc8
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2021-03-11 00:37:24 +0100

    Рефакторинг

--- user-functions.php
+++ user-functions.php
@@ -3,22 +3,21 @@
 /**
  * Функция, которая возвращает результат: оригинальный текст, если его длина меньше
  * заданного числа символов. В противном случае это урезанный текст с прибавленной к нему ссылкой.
- * @param $string
- * @param int $symbols
+ * @param string $string
+ * @param int $max_length
+ * @return string
  */
-function cut_string($string, $symbols = 300) {
+function cut_string(string $string, int $max_length = 300): string {
 
-    if (mb_strlen($string) <= $symbols) return $string;
+    if (mb_strlen($string) <= $max_length) return $string;
 
     $words = explode(' ', $string);
     $result_string = '';
 
     foreach ($words as $word) {
-        if (mb_strlen($result_string . ' ' . $word) > $symbols) break;
+        if (mb_strlen($result_string . ' ' . $word) > $max_length) break;
 
-        if (!$result_string) $result_string .= $word;
-
-        $result_string .= ' ' . $word;
+        $result_string .= !$result_string ? $word :  ' ' . $word;
     }
 
     return $result_string . '...';
